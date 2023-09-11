9d4ab45b04ca513daec56e249f670f0cde591305
GnsYnns <135223509+GnsYnns@users.noreply.github.com>
2023-09-11 12:26:22 +0200

    Update blog.sql

--- MVC/v0 - Yannis/blog.sql
+++ MVC/v0 - Yannis/blog.sql
@@ -4,16 +4,16 @@ CREATE DATABASE IF NOT EXISTS blog CHARSET=utf8 COLLATE=utf8_general_ci;
 USE blog;
 
 CREATE TABLE Article(
-   id_article INT,
+   id INT,
    auteur VARCHAR(70),
-   date_article DATETIME,
+   date_ DATETIME,
    titre VARCHAR(100) NOT NULL,
    contenu TEXT NOT NULL,
    PRIMARY KEY(id_article)
 );
 
 CREATE TABLE Commentaire(
-   id_comentaire INT,
+   id INT,
    date_commentaire DATETIME,
    auteur VARCHAR(70) NOT NULL,
    contenu TEXT NOT NULL,
