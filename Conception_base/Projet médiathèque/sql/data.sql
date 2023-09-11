21746a7167ae2573015a673786e14d7c7113edc8
icfsio2022 <143408287+icfsio2022@users.noreply.github.com>
2023-09-11 11:38:11 +0200

    Add files via upload

---
+++ Conception_base/Projet médiathèque/sql/data.sql
@@ -0,0 +1,11 @@
+-- Création de la bdd
+source biblio.sql
+
+-- Remplissage de la bdd
+source pays.sql
+source adherent.sql
+source auteur.sql
+source editeur.sql
+source media.sql
+source exemplaire.sql
+source pret.sql
